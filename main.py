17f43d05ec656a961c11980a287ab0ef49abf98b
Marco <patrickstar1281@yahoo.com>
2023-12-28 14:32:43 -0600

    application setup

---
+++ main.py
@@ -0,0 +1,77 @@
+from dotenv import load_dotenv
+import os
+import requests
+import urllib.parse
+from datetime import datetime, timedelta
+from flask import Flask, redirect, request, jsonify, session
+
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+
+AUTH_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+API_BASE_URL = "https://api.spotify.com/v1"
+
+@app.route('/')
+def index():
+    return "Welcome to my first spotify app! <a href='/login'>Login with Your Spotify</a>"
+
+@app.route("/login")
+def login():
+    scope = 'user-read-private user-read-email'
+    params = {
+        'client_id': CLIENT_ID,
+        'respomse_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI,
+        # comment below out in production for testing only.*
+        'show_dialog': True
+    }
+
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+
+        response = requests.post(TOKEN_URL, data=req_body)
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expies_in']
+
+        return redirect('/playlists')
+    
+@app.route('/playlists')
+def get_playlists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+    
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
